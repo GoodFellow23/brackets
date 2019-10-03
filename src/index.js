42b4d723a7ba3a6a13c9e32f6e5ff6ff5c416c10
Dima <yackevit4dima@gmail.com>
2019-10-03 14:07:31 +0300

    first changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+              
+    const brackLength=bracketsConfig[0].length;
+    var tempOpen=0;
+    var tempClose=0;
+    var temp=0;
+    switch (brackLength) {
+        case 1:
+            for(var i=0;i<str.length;i++)
+            {
+                if(str[i]==bracketsConfig[0][0])temp++;
+                else(str[i]==bracketsConfig[0][1])
+                {
+                    for(var j=0;j<temp;j++)
+                    {
+                        if(a[i+j]==bracketsConfig[0][1])tempClose++;
+                        
+                    }
+                    i+=j;
+                    tempOpen=temp;
+                    temp=0;
+                }
+            }
+            if(tempOpen==tempClose)return true;
+            return false;
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            
+            break;
+        
+    }
+       
+        
+     
+
+   
 }
+check();
